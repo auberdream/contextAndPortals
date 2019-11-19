9003244e5f48165100773baa89dd0cfd7092118b
Ellie Wem <ellie.wem@rainbird.ai>
2019-11-19 12:34:00 +0000

    chore: add header

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+import logo from "../../resources/images/logo.svg";
+
+export default () => (
+  <header className={'header'}>
+    <div className={'header__item'}>
+      <img src={logo} className="header__logo" alt="logo" />
+    </div>
+    <div className={'header__item header__item-expandable'}>
+      <span>Mid content</span>
+    </div>
+    <div className={'header__item'}>
+      <span>x</span>
+    </div>
+  </header>
+)
